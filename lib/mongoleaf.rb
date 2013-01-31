6abc89194c05ccbf7ed132084523484cf7282fa6
zeroed <edd.rossi@gmail.com>
2013-01-31 23:31:59 +0100

    coding the API

--- lib/mongoleaf.rb
+++ lib/mongoleaf.rb
@@ -3,11 +3,10 @@ require "mongoleaf/poster"
 require "mongoleaf/util"
 
 module Mongoleaf
-  # Your code goes here...
   include Poster
   include Util
   def self.included(klass)
-    p "#{Time.now} #{self} included"
+    # p "#{Time.now} #{self} included"
     # klass.extend(self)
   end
 end
--- lib/mongoleaf/poster.rb
+++ lib/mongoleaf/poster.rb
@@ -32,12 +32,24 @@ module Mongoleaf::Poster
       @url = URI.parse "#{Constants::Endpoint}#{key}"
     end
 
-    def collection database
+    def databases_subservices database
+      # /databases/<d>
+      # GET - lists sub-services for database <d>
+    end
+
+    def collections database
       # /databases/<d>/collections
       # GET - lists collections in database <d>
       @url = URI.parse "#{Constants::Endpoint}#{database}/collections/#{key}"
     end
    
+    def collection database, collection
+      # /databases/<d>/collections/<c>
+      # GET - lists objects in collection <c>
+      # POST - inserts a new object into collection <c>
+      @url = URI.parse "#{Constants::Endpoint}#{database}/collections/#{collection}#{key}"
+    end
+
     def id(database, collection, id, hash={})
       # /databases/<d>/collections/<c>/<id>
       # GET - returns object with _id <id>
@@ -46,40 +58,36 @@ module Mongoleaf::Poster
       @url = URI.parse "#{Constants::Endpoint}#{database}/collections/#{collection}/id/#{key}"
     end
 
-    def library_master hash={}
+    def library_master
       @url = URI.parse "#{Constants::Endpoint}/bookmarks/collections/library/master/#{key}"
     end
+    
+    def select db="bookmarks", coll="library", q={'id'=>'master'}
+      # /databases/<db>/collections/<coll>?[q=<query>][&c=true] [&f=<fields>][&fo=true][&s=<order>] [&sk=<skip>][&l=<limit>]
+      # GET - lists all objects matching these optional params:
+      # q: JSON queryreference
+      # c: returns the result count for this query
+      # f: set of fields to be returned in each object
+      #    (1—include; 0—exclude)
+      #    e.g. { "name" : 1, "email": 1 } OR 
+      #    e.g. { "comments" : 0 } 
+      # fo: return a single object from the result set
+      #    (same as 'findOne()' in the mongo shell) 
+      # s: sort order (1—asc; -1—desc) 
+      #    i.e. { <field> : <order> }
+      # sk: number of results to skip in the result set
+      # l: limit for the number of results
+    end
 
-=begin
-
-/databases/<d>
-  GET - lists sub-services for database <d>
-/databases/<d>/collections/<c>
-  GET - lists objects in collection <c>
-  POST - inserts a new object into collection <c>
-/databases/<d>/collections/<c>?[q=<query>][&c=true] [&f=<fields>][&fo=true][&s=<order>] [&sk=<skip>][&l=<limit>]
-  GET - lists all objects matching these
-        optional params:
-    q: JSON queryreference
-    c: returns the result count for this query
-    f: set of fields to be returned in each object
-       (1—include; 0—exclude)
-       e.g. { "name" : 1, "email": 1 } OR 
-       e.g. { "comments" : 0 } 
-   fo: return a single object from the result set
-       (same as 'findOne()' in the mongo shell) 
-    s: sort order (1—asc; -1—desc) 
-       i.e. { <field> : <order> }
-   sk: number of results to skip in the result set
-    l: limit for the number of results
-/databases/<d>/collections/<c>?[q=<query>][&m=true] [&u=true]
-  PUT - updates one or all objects matching the query.
-        payload should contain modifier operations
-    q: JSON queryreference
-    m: apply update to all objects in result set
-       (by default, only one is updated)
-    u: insert if none match the query (upsert)
-=end
+    def insert db="bookmarks", coll="library", q={'id'=>'master'} 
+      # /databases/<d>/collections/<c>?[q=<query>][&m=true] [&u=true]
+      # PUT - updates one or all objects matching the query.
+      #   payload should contain modifier operations
+      # q: JSON queryreference
+      # m: apply update to all objects in result set
+      #  (by default, only one is updated)
+      # u: insert if none match the query (upsert)
+    end
 
     def get
       net = Net::HTTP.new(@url.host, @url.port)
@@ -101,6 +109,18 @@ module Mongoleaf::Poster
       request = Net::HTTP::Post.new(@url.path + '?' + @url.query)
       request.content_type = 'application/json'
       request.body = hash.to_json
+      response = net.start {|http| http.request(request)}
+      case response
+        when Net::HTTPBadResponse
+          puts "Net::dResponse ??"
+        else
+          puts "Response code: #{response.code} message: #{response.message} body: #{response.body}"
+      end
+      rescue SocketError
+        puts "SocketError rescued"
+    end
+
+    def get_response 
     end
 
     def dump
@@ -109,17 +129,13 @@ module Mongoleaf::Poster
       JSON.dump(@bookmarks, io)
     end
 
-    def connect_and_send(hash, key = "")
-      unless key
-        File.open("./config/api-key","r").each_line {|l| key = l; key.chomp!}
-      end
-      path = ("https://api.mongolab.com/api/1/databases/bookmarks/collections/notes?apiKey="\
-       + key)
+    def post_note(hash, key)
+        File.open("./config/api-key","r").each_line {|l| key = l; key.chomp!} unless key
+      path = "https://api.mongolab.com/api/1/databases/bookmarks/collections/notes?apiKey=#{key}"
       uri = URI.parse(path)
       net = Net::HTTP.new(uri.host, uri.port)
       net.use_ssl = true
-      path_and_query = uri.path + '?' + uri.query
-      request = Net::HTTP::Post.new(path_and_query)
+      request = Net::HTTP::Post.new("#{uri.path}?#{uri.query}")
       request.content_type = 'application/json'
       request.body = hash.to_json
       response = net.start {|http| http.request(request)}
